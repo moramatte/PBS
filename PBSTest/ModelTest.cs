6f702909634a237788189db2337e0daf1c50af3a
moramatte <moramatte@gmail.com>
2017-03-21 13:45:57 +0100

    Adding test

---
+++ PBSTest/ModelTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PBS;
+
+namespace PBSTest
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subject = new Model();
+            Assert.AreEqual("Jim", subject.Name);
+        }
+    }
+}
---
+++ PBSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PBSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PBSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1de754d8-d543-41cf-a7e2-ce05a0edddd1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
